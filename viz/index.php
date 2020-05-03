7194744dd021456cabeda8d5e6e277cd6ad0cef5
Rushil Shakya <rushil.shakya@gmail.com>
2020-05-04 00:01:39 +0545

    feat: demo file

--- viz/index.php
+++ viz/index.php
@@ -1,8 +1,3 @@
-<?php
- require_once('../../../../wp-config.php');
-	global $wpdb;
-	$na_table_name = $wpdb->prefix . 'na_covid19Nepal';
-	?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,7 +12,7 @@
 		</span>
 
 		<table id="nepalis-abroad" border=1px>
-			<tr>
+			<tr style="background-color:DeepSkyBlue; color: white">
 				<td>country</td>
 				<td>region</td>
 				<td>infected</td>
@@ -26,30 +21,14 @@
 			</tr>
 		</table>
 
-	<?php
-		echo '<script type="text/javascript">';
-		//START
-		$naSql = "SELECT *
-								FROM $na_table_name
-								WHERE groupDate = (SELECT MAX(groupDate) FROM $na_table_name)
-						ORDER BY region, country ASC";
+		<script type="text/javascript" src="/wp-content/plugins/C19nepalisAbroadData/js/nepalisAbroad.js"></script>
 
-		$naResults= $wpdb->get_results($naSql);
-
-		echo 'var covidNepalisAbroad=[];';
-
-		foreach ( $naResults as $result ) {
-			echo 'covidNepalisAbroad.push(' . json_encode($result) . ');';
-		}
-		//END
-		echo "</script>";
-	?>
 		<script>
 			const table = document.getElementById('nepalis-abroad');
 
-			for(let i=0; i<covidNepalisAbroad.length; i++) {
+			for(let i=0; i<covidNepalisAbroad.countries.length; i++) {
 				const trEl = document.createElement('tr');
-				const oneRow = covidNepalisAbroad[i];
+				const oneRow = covidNepalisAbroad.countries[i];
 
 				const tdCtry = document.createElement('td');
 				tdCtry.innerHTML = oneRow.country;
@@ -73,6 +52,33 @@
 
 				table.appendChild(trEl);
 			}
+
+			const trEl = document.createElement('tr');
+			const totRow = covidNepalisAbroad.total;
+
+			const tdCtry = document.createElement('td');
+			tdCtry.innerHTML = "TOTAL";
+			trEl.appendChild(tdCtry);
+
+			const tdRgn = document.createElement('td');
+			trEl.appendChild(tdRgn);
+
+			const tdInf = document.createElement('td');
+			tdInf.innerHTML = totRow.infected;
+			trEl.appendChild(tdInf);
+
+			const tdFat = document.createElement('td');
+			tdFat.innerHTML = totRow.fatality;
+			trEl.appendChild(tdFat);
+
+			const tdRec = document.createElement('td');
+			tdRec.innerHTML = totRow.recovered;
+			trEl.appendChild(tdRec);
+
+			trEl.style.backgroundColor="grey"
+			trEl.style.color="white"
+
+			table.appendChild(trEl);
 		</script>
 	</body>
 </html>
