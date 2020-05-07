041faca66a83f41cd188134c5426a90aa1a543c1
Rushil Shakya <rushil.shakya@gmail.com>
2020-05-07 19:44:43 +0545

    feat: disable unused vars

--- js/nepalisAbroad.js
+++ js/nepalisAbroad.js
@@ -1,172 +1,200 @@
-var covidNepalisAbroad=
-										{
-											asOfDate: "2020-05-06",
-											total: {
-					infected: "5324",
-					fatality: "73",
-					recovered: "1579"
-				},
-											countries: [{
-					country: "Australia",
-					region: "Oceania",
-					infected: "11",
-					fatality: "",
-					recovered: ""
-				},{
-					country: "Bahrain",
-					region: "Middle East",
-					infected: "126",
-					fatality: "",
-					recovered: "50"
-				},{
-					country: "Belgium",
-					region: "Europe",
-					infected: "6",
-					fatality: "",
-					recovered: ""
-				},{
-					country: "Canada",
-					region: "Americas",
-					infected: "5",
-					fatality: "",
-					recovered: "4"
-				},{
-					country: "Cyprus",
-					region: "Europe",
-					infected: "3",
-					fatality: "",
-					recovered: ""
-				},{
-					country: "France",
-					region: "Europe",
-					infected: "1",
-					fatality: "",
-					recovered: ""
-				},{
-					country: "Germany",
-					region: "Europe",
-					infected: "4",
-					fatality: "",
-					recovered: ""
-				},{
-					country: "Hong Kong",
-					region: "Asia",
-					infected: "2",
-					fatality: "",
-					recovered: ""
-				},{
-					country: "Ireland",
-					region: "Europe",
-					infected: "22",
-					fatality: "1",
-					recovered: "22"
-				},{
-					country: "Japan",
-					region: "Asia",
-					infected: "6",
-					fatality: "1",
-					recovered: "1"
-				},{
-					country: "Kuwait",
-					region: "Middle East",
-					infected: "78",
-					fatality: "",
-					recovered: ""
-				},{
-					country: "Malaysia",
-					region: "Asia",
-					infected: "3",
-					fatality: "",
-					recovered: ""
-				},{
-					country: "Maldives",
-					region: "Asia",
-					infected: "2",
-					fatality: "",
-					recovered: ""
-				},{
-					country: "Malta",
-					region: "Europe",
-					infected: "2",
-					fatality: "",
-					recovered: ""
-				},{
-					country: "Myanmar",
-					region: "Asia",
-					infected: "1",
-					fatality: "",
-					recovered: ""
-				},{
-					country: "Nepal",
-					region: "Asia",
-					infected: "75",
-					fatality: "",
-					recovered: "16"
-				},{
-					country: "New Zealand",
-					region: "Oceania",
-					infected: "3",
-					fatality: "",
-					recovered: "2"
-				},{
-					country: "Portugal",
-					region: "Europe",
-					infected: "27",
-					fatality: "",
-					recovered: "3"
-				},{
-					country: "Qatar",
-					region: "Middle East",
-					infected: "2300",
-					fatality: "",
-					recovered: "700"
-				},{
-					country: "Saudi Arabia",
-					region: "Middle East",
-					infected: "30",
-					fatality: "",
-					recovered: "3"
-				},{
-					country: "Spain",
-					region: "Europe",
-					infected: "12",
-					fatality: "",
-					recovered: "10"
-				},{
-					country: "Switzerland",
-					region: "Europe",
-					infected: "5",
-					fatality: "",
-					recovered: "3"
-				},{
-					country: "The Netherlands",
-					region: "Europe",
-					infected: "",
-					fatality: "1",
-					recovered: ""
-				},{
-					country: "Turkey",
-					region: "Europe",
-					infected: "",
-					fatality: "1",
-					recovered: ""
-				},{
-					country: "United Arab Emirates",
-					region: "Middle East",
-					infected: "100",
-					fatality: "5",
-					recovered: "15"
-				},{
-					country: "United Kingdom",
-					region: "Europe",
-					infected: "1000",
-					fatality: "49",
-					recovered: "250"
-				},{
-					country: "USA",
-					region: "Americas",
-					infected: "1500",
-					fatality: "15",
-					recovered: "500"
-				}]
-										};
+/* eslint-disable no-unused-vars */
+var covidNepalisAbroad = {
+  asOfDate: '2020-05-06',
+  total: {
+    infected: '5324',
+    fatality: '73',
+    recovered: '1579',
+  },
+  countries: [
+    {
+      country: 'Australia',
+      region: 'Oceania',
+      infected: '11',
+      fatality: '',
+      recovered: '',
+    },
+    {
+      country: 'Bahrain',
+      region: 'Middle East',
+      infected: '126',
+      fatality: '',
+      recovered: '50',
+    },
+    {
+      country: 'Belgium',
+      region: 'Europe',
+      infected: '6',
+      fatality: '',
+      recovered: '',
+    },
+    {
+      country: 'Canada',
+      region: 'Americas',
+      infected: '5',
+      fatality: '',
+      recovered: '4',
+    },
+    {
+      country: 'Cyprus',
+      region: 'Europe',
+      infected: '3',
+      fatality: '',
+      recovered: '',
+    },
+    {
+      country: 'France',
+      region: 'Europe',
+      infected: '1',
+      fatality: '',
+      recovered: '',
+    },
+    {
+      country: 'Germany',
+      region: 'Europe',
+      infected: '4',
+      fatality: '',
+      recovered: '',
+    },
+    {
+      country: 'Hong Kong',
+      region: 'Asia',
+      infected: '2',
+      fatality: '',
+      recovered: '',
+    },
+    {
+      country: 'Ireland',
+      region: 'Europe',
+      infected: '22',
+      fatality: '1',
+      recovered: '22',
+    },
+    {
+      country: 'Japan',
+      region: 'Asia',
+      infected: '6',
+      fatality: '1',
+      recovered: '1',
+    },
+    {
+      country: 'Kuwait',
+      region: 'Middle East',
+      infected: '78',
+      fatality: '',
+      recovered: '',
+    },
+    {
+      country: 'Malaysia',
+      region: 'Asia',
+      infected: '3',
+      fatality: '',
+      recovered: '',
+    },
+    {
+      country: 'Maldives',
+      region: 'Asia',
+      infected: '2',
+      fatality: '',
+      recovered: '',
+    },
+    {
+      country: 'Malta',
+      region: 'Europe',
+      infected: '2',
+      fatality: '',
+      recovered: '',
+    },
+    {
+      country: 'Myanmar',
+      region: 'Asia',
+      infected: '1',
+      fatality: '',
+      recovered: '',
+    },
+    {
+      country: 'Nepal',
+      region: 'Asia',
+      infected: '75',
+      fatality: '',
+      recovered: '16',
+    },
+    {
+      country: 'New Zealand',
+      region: 'Oceania',
+      infected: '3',
+      fatality: '',
+      recovered: '2',
+    },
+    {
+      country: 'Portugal',
+      region: 'Europe',
+      infected: '27',
+      fatality: '',
+      recovered: '3',
+    },
+    {
+      country: 'Qatar',
+      region: 'Middle East',
+      infected: '2300',
+      fatality: '',
+      recovered: '700',
+    },
+    {
+      country: 'Saudi Arabia',
+      region: 'Middle East',
+      infected: '30',
+      fatality: '',
+      recovered: '3',
+    },
+    {
+      country: 'Spain',
+      region: 'Europe',
+      infected: '12',
+      fatality: '',
+      recovered: '10',
+    },
+    {
+      country: 'Switzerland',
+      region: 'Europe',
+      infected: '5',
+      fatality: '',
+      recovered: '3',
+    },
+    {
+      country: 'The Netherlands',
+      region: 'Europe',
+      infected: '',
+      fatality: '1',
+      recovered: '',
+    },
+    {
+      country: 'Turkey',
+      region: 'Europe',
+      infected: '',
+      fatality: '1',
+      recovered: '',
+    },
+    {
+      country: 'United Arab Emirates',
+      region: 'Middle East',
+      infected: '100',
+      fatality: '5',
+      recovered: '15',
+    },
+    {
+      country: 'United Kingdom',
+      region: 'Europe',
+      infected: '1000',
+      fatality: '49',
+      recovered: '250',
+    },
+    {
+      country: 'USA',
+      region: 'Americas',
+      infected: '1500',
+      fatality: '15',
+      recovered: '500',
+    },
+  ],
+};
